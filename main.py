5ea25cb7141bc9d85c61dd2763f1ab47d533a8bf
masumi uozumi <41750783+RyutoThurusaki@users.noreply.github.com>
2026-03-20 16:04:33 +0900

    起動処理追加

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 from machine import Pin
+import machine
 import time
+import utime
+import network
+import slack_sdk
 
-# 起動確認
+# require "Pico W"
+SSID = 'SSID'
+PW = 'Password'
+
+# Require SlackAPI https://qiita.com/odm_knpr0122/items/04c342ec8d9fe85e0fe9
+# xoxb-xxxxxxxxxxxxx-xxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx
+Token = 'Token'
+Channel = '#general'
+
+# wifi接続確認＆起動通知
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, PW)
+
+while wlan.isconnected() == False:
+    client = slack_sdk.WebClient(Token)
+    client.chat_postMessage(Channel, text="RainDetectionが起動しました")
+
+# 起動LED
 led = machine.Pin(25, machine.Pin.OUT)
 led.value(1)
 time.sleep(5)
 led.value(0)
 
+
+while True:
 # Dormant Modeでスリープする。雨に反応して起動する。
-
+    
 # Wifi起動して通知
-
-# スリープモードに戻り、10分おきに疎通確認
-
+    
+# wifi解除
+    
+# lightsleepに入り、10分おきに疎通確認。
+    
 # 疎通が解除されたら
+
+end

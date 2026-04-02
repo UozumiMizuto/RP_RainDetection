a9a8e862543c4175d81e394ded19efe635aa60f7
masumi uozumi <41750783+RyutoThurusaki@users.noreply.github.com>
2026-04-02 16:10:30 +0900

    slack処理を関数にパック

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ import json
 
 import secrets
 
+#####################################
+# wifi
+wlan = network.WLAN(network.STA_IF)
+
 # slack
 def send_slack(text):
+    connect_wifi()
+    
     data = {"text": text}
     payload = json.dumps(data).encode('utf-8')
     headers = {"Content-Type": "application/json"}
@@ -21,19 +27,27 @@ def send_slack(text):
         response.close()
     except Exception as e:
         print("Error:", e)
-        
-        
-# wifi接続確認＆起動通知
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(secrets.SSID, secrets.PW)
-
-while not wlan.isconnected():
-    time.sleep(1)
     
+    wlan.active(False)
+    wlan.deinit()
+        
+# wifi接続処理　直接呼び出したりしない。
+def connect_wifi():
+    # wifi接続確認＆起動通知
+    wlan.active(True)
+    wlan.connect(secrets.SSID, secrets.PW)
+
+    while not wlan.isconnected():
+        time.sleep(1)
+        
+    status = wlan.ifconfig()
+    print(status[0])
+    
+#####################################
+
+# 起動時ここから開始
 send_slack("RainDetectionが起動しました")
-status = wlan.ifconfig()
-print(status[0])
+        
 
 # 起動LED
 led = machine.Pin("LED", machine.Pin.OUT)
@@ -43,9 +57,6 @@ led.value(0)
 
 
 while True:
-    wlan.active(False)
-    wlan.deinit()
-    
     print("aaaa")
 # 1. 起こしてくれるピンを指定（例：GP15）
     wake_up_pin = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_DOWN)
@@ -57,19 +68,7 @@ while True:
 
 # --- 15番ピンに電気が流れると、ここから再開 ---
     print("おはよう！")
-    
-    wlan.active(True)
-    while not wlan.isconnected():
-        time.sleep(1)
-        
     send_slack("雨が降ってきました")
-
-# Dormant Modeでスリープする。雨に反応して起動する。
-
-# Wifi起動して通知
-    
-# wifi解除
-    
 # lightsleepに入り、10分おきに疎通確認。
     
 # 疎通が解除されたら
